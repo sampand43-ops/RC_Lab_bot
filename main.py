cd2601dc2fd020560e483c0617669e687e956a6e
sampand43-ops <sampand43@gmail.com>
2026-08-13 04:10:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,9 @@ from pyrogram.errors import FloodWait
 # استخدم نفس القيم الموجودة في كودك الأصلي.
 # الأفضل وضعها في Railway Variables بالأسماء:
 # API_ID / API_HASH / TOKEN
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-TOKEN = os.getenv("TOKEN", "")
-
+API_ID = int(os.getenv("34123643", "0"))
+API_HASH = os.getenv("12dccc6e1dce1c82853587ba04e9694d", "")
+TOKEN = os.getenv("8619586974:AAGuSahN1tsDZLNOtmSOmdjwjw8ZcC2IMe8", "")
 
 # ============================================================
 # Channel
